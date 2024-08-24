55ef8f9b33370cd82b10d9c64f8c634daca65a90
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2024-08-24 17:54:22 +0800

    Completed insertLeaf() method in week2 assignment (#15)
    * Completed CheckRoot() function
    
    * Added MerkleTree circom logics
    
    * using Mux()
    
    * updated
    
    * completed insertLeaf

--- 2024/my-responses/week-2/zku/Part1/circuits/MerkleTree.circom
+++ 2024/my-responses/week-2/zku/Part1/circuits/MerkleTree.circom
@@ -1,19 +1,54 @@
-pragma circom 2.0.0;
+pragma circom 2.1.8;
 
 include "../node_modules/circomlib/circuits/poseidon.circom";
+include "../node_modules/circomlib/circuits/mux1.circom";
 
 template CheckRoot(n) { // compute the root of a MerkleTree of n Levels 
-    signal input leaves[2**n];
-    signal output root;
+  signal input leaves[2**n];
+  signal output root;
+  signal nodes[2**n];
 
-    //[assignment] insert your code here to calculate the Merkle root from 2^n leaves
+  //[assignment] insert your code here to calculate the Merkle root from 2^n leaves
+  component hasher2[2**n];
+
+  var accum = 0;
+  var currLayerOffset = 0;
+  var nextLayerNum = 2**n;
+  while (nextLayerNum > 1) {
+    nextLayerNum /= 2;
+    for (var i = 0; i < nextLayerNum; i++) {
+      hasher2[accum + i] = Poseidon(2);
+      hasher2[accum + i].inputs[0] <== accum == 0 ? leaves[2*i] : nodes[currLayerOffset + 2*i];
+      hasher2[accum + i].inputs[1] <== accum == 0 ? leaves[2*i + 1] : nodes[currLayerOffset + 2*i + 1];
+      nodes[accum + i] <== hasher2[accum + i].out;
+    }
+    currLayerOffset = accum;
+    accum += nextLayerNum;
+  }
+  root <== nodes[currLayerOffset];
 }
 
 template MerkleTreeInclusionProof(n) {
-    signal input leaf;
-    signal input path_elements[n];
-    signal input path_index[n]; // path index are 0's and 1's indicating whether the current element is on the left or right
-    signal output root; // note that this is an OUTPUT signal
+  signal input leaf;
+  signal input path_elements[n];
+  signal input path_index[n]; // path index are 0's and 1's indicating whether the current element is on the left or right
+  signal output root; // note that this is an OUTPUT signal
 
-    //[assignment] insert your code here to compute the root from a leaf and elements along the path
+  //[assignment] insert your code here to compute the root from a leaf and elements along the path
+  signal nodes[n + 1];
+  component hasher2[n];
+  component muxes[n];
+
+  nodes[0] <== leaf;
+  for (var i = 0; i < n; i++) {
+    hasher2[i] = Poseidon(2);
+    muxes[i] = Mux1();
+    muxes[i].c[0] <== path_elements[i];
+    muxes[i].c[1] <== nodes[i];
+    muxes[i].s <== path_index[i];
+    hasher2[i].inputs[0] <== muxes[i].out;
+    hasher2[i].inputs[1] <== nodes[i] + path_elements[i] - muxes[i].out;
+    nodes[i+1] <== hasher2[i].out;
+  }
+  root <== nodes[n];
 }
--- 2024/my-responses/week-2/zku/Part1/contracts/MerkleTree.sol
+++ 2024/my-responses/week-2/zku/Part1/contracts/MerkleTree.sol
@@ -4,26 +4,66 @@ pragma solidity ^0.8.0;
 import { PoseidonT3 } from "./Poseidon.sol"; //an existing library to perform Poseidon hash on solidity
 import "./verifier.sol"; //inherits with the MerkleTreeInclusionProof verifier contract
 
-contract MerkleTree is Verifier {
-    uint256[] public hashes; // the Merkle tree in flattened array form
-    uint256 public index = 0; // the current index of the first unfilled leaf
-    uint256 public root; // the current Merkle root
+contract MerkleTree is Groth16Verifier {
+  using PoseidonT3 for uint256[2];
 
-    constructor() {
-        // [assignment] initialize a Merkle tree of 8 with blank leaves
+  uint16 public constant TREE_LV = 3; // The root starts from lv 0, and grow downward.
+  uint256[] public hashes; // the Merkle tree in flattened array form
+  uint256 public index = 0; // the current index of the first unfilled leaf
+  uint256 public root; // the current Merkle root
+
+  // Errors
+  error MerkleTreeIsFull();
+
+  // Events
+
+  constructor() {
+    // [assignment] initialize a Merkle tree of 8 with blank leaves
+    uint256 val = 0;
+    uint256 leafNum = uint256(2 ** TREE_LV);
+    for (uint256 nodeNum = leafNum; nodeNum > 0; nodeNum /= 2) {
+      val = nodeNum == leafNum ? 0 : [val, val].poseidon();
+      _pushMultiples(hashes, val, nodeNum);
+    }
+  }
+
+  function _pushMultiples(uint256[] storage arr, uint256 el, uint256 times) private {
+    for (uint256 i = 0; i < times; i++) {
+      arr.push(el);
+    }
+  }
+
+  function insertLeaf(uint256 hashedLeaf) public returns (uint256) {
+    // [assignment] insert a hashed leaf into the Merkle tree
+    uint256 maxLeafIdx = uint256((2 ** TREE_LV) - 1);
+    if (index > maxLeafIdx) revert MerkleTreeIsFull();
+
+    // uint256 nodeNum = uint256((2 ** (TREE_LV + 1)) - 1)
+    uint256 remainder = index;
+    uint256 newHash = hashedLeaf;
+    uint256 accum = 0; // this is the index of the lefthand most node on that layer
+    for (int16 lv = int16(TREE_LV); lv >= 0; --lv) {
+      uint256 idx = accum + remainder;
+      hashes[idx] = newHash;
+      if (lv > 0) {
+        // we don't need the following computation as we are at the root level already
+        newHash = idx % 2 == 0
+          ? [hashes[idx], hashes[idx+1]].poseidon()
+          : [hashes[idx-1], hashes[idx]].poseidon();
+        accum += (2 ** uint16(lv));
+        remainder /= 2;
+      }
     }
 
-    function insertLeaf(uint256 hashedLeaf) public returns (uint256) {
-        // [assignment] insert a hashed leaf into the Merkle tree
-    }
+    return index++;
+  }
 
-    function verify(
-            uint[2] memory a,
-            uint[2][2] memory b,
-            uint[2] memory c,
-            uint[1] memory input
-        ) public view returns (bool) {
-
-        // [assignment] verify an inclusion proof and check that the proof root matches current root
-    }
+  function verify(
+    uint[2] memory a,
+    uint[2][2] memory b,
+    uint[2] memory c,
+    uint[1] memory input
+  ) public view returns (bool) {
+    // [assignment] verify an inclusion proof and check that the proof root matches current root
+  }
 }
