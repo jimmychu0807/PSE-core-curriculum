828bef24e5f0b0c6bfbf805ca311c82f78be8d0f
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2024-08-24 17:53:38 +0800

    completed insertLeaf

--- 2024/my-responses/week-2/zku/Part1/contracts/MerkleTree.sol
+++ 2024/my-responses/week-2/zku/Part1/contracts/MerkleTree.sol
@@ -20,28 +20,40 @@ contract MerkleTree is Groth16Verifier {
   constructor() {
     // [assignment] initialize a Merkle tree of 8 with blank leaves
     uint256 val = 0;
-    uint32 leafNum = uint32(2 ** TREE_LV);
-    for (uint32 nodeNum = leafNum; nodeNum > 0; nodeNum /= 2) {
+    uint256 leafNum = uint256(2 ** TREE_LV);
+    for (uint256 nodeNum = leafNum; nodeNum > 0; nodeNum /= 2) {
       val = nodeNum == leafNum ? 0 : [val, val].poseidon();
       _pushMultiples(hashes, val, nodeNum);
     }
   }
 
-  function _pushMultiples(uint256[] storage arr, uint256 el, uint32 times) private {
-    for (uint32 i = 0; i < times; i++) {
+  function _pushMultiples(uint256[] storage arr, uint256 el, uint256 times) private {
+    for (uint256 i = 0; i < times; i++) {
       arr.push(el);
     }
   }
 
   function insertLeaf(uint256 hashedLeaf) public returns (uint256) {
     // [assignment] insert a hashed leaf into the Merkle tree
-    uint32 maxLeafIdx = uint32((2 ** TREE_LV) - 1);
-    if (index > maxLeafIdx) {
-      revert MerkleTreeIsFull();
-    }
+    uint256 maxLeafIdx = uint256((2 ** TREE_LV) - 1);
+    if (index > maxLeafIdx) revert MerkleTreeIsFull();
 
-    hashes[index] = hashedLeaf;
-    // Something on traversing up the tree
+    // uint256 nodeNum = uint256((2 ** (TREE_LV + 1)) - 1)
+    uint256 remainder = index;
+    uint256 newHash = hashedLeaf;
+    uint256 accum = 0; // this is the index of the lefthand most node on that layer
+    for (int16 lv = int16(TREE_LV); lv >= 0; --lv) {
+      uint256 idx = accum + remainder;
+      hashes[idx] = newHash;
+      if (lv > 0) {
+        // we don't need the following computation as we are at the root level already
+        newHash = idx % 2 == 0
+          ? [hashes[idx], hashes[idx+1]].poseidon()
+          : [hashes[idx-1], hashes[idx]].poseidon();
+        accum += (2 ** uint16(lv));
+        remainder /= 2;
+      }
+    }
 
     return index++;
   }
